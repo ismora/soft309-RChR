c075554c5f7cdfee7d6050d7c3bd0323fc03efa3
Rebeca Chacon Rodriguez <rebevic1992@gmail.com>
2025-04-24 21:01:18 -0600

    [update] Modificación

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from src.operations import suma, resta
 
 def test_suma():
-    assert suma(2, 3) == 5
+    assert suma(2, 6) == 5
 
 def test_resta():
-    assert resta(5, 3) == 2
+    assert resta(5, 7) == 2
